738e04302c2a8ffc42f383235bb3e53f18297e20
GreeN <xd383727@gamil.com>
2023-07-24 21:06:40 +0300

    create model challenge

--- api/models/model.py
+++ api/models/model.py
@@ -1,7 +1,13 @@
 from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import String, Integer, Column, Text
 
 Base = declarative_base()
 
 
 class Challenge(Base):
     __tablename__ = "challenge"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(Text)
+    
