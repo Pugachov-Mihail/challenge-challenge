7e041fe60489f86161a128ae05e6c1312534ccef
xd383727@gmail.com <@!lodS9506>
2023-07-28 14:51:10 +0300

    create model challenge, day point, day purpose

--- api/config/config_db.py
+++ api/config/config_db.py
@@ -3,19 +3,16 @@ from typing import Generator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-import settings
+from . import settings
 
 engine = create_async_engine(settings.DATABASE_URL, future=True, echo=True)
 
-async_session: AsyncSession = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession)
+async_session = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession)
 
 
-async def get_db() -> Generator:
-    try:
-        session: AsyncSession = async_session()
+async def get_db() -> AsyncSession:
+    async with async_session() as session:
         yield session
-    except Exception:
-        raise Exception
-    finally:
-        await session.close()
